c15adb91d17b7455ca723a9a2e4c80ceaa301a3e
Noémie ROCHARD <nrochard@student.42.fr>
2022-04-22 09:19:13 +0200

    add color

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
+
   "stories": [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "storybook-design-token"
   ],
   "framework": "@storybook/html"
 }
--- stories/Headline.stories.js
+++ stories/Headline.stories.js
@@ -2,7 +2,7 @@ import { createHeadline } from './Headline';
 
 // More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Headline',
+  title: 'Tokens/Headline',
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
     label: { control: 'text' },
